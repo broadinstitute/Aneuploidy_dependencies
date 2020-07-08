33710aa5205b9d04dd0b66f82a4cbd27e97184d5
mkazachkova2 <42276716+mkazachkova2@users.noreply.github.com>
2020-07-08 17:38:04 -0400

    cleaned and run_lm_stats in separate file

--- Limma/drug_comparisons/using_limma_GDSC.R
+++ Limma/drug_comparisons/using_limma_GDSC.R
@@ -10,12 +10,9 @@ install.packages('cdsr')
 library(taigr)
 
 # want to use secondary screen data
-#secondary.dose.response.curve.parameters <- load.from.taiga(data.name='secondary-screen-0854', data.version=13, data.file='secondary_dose_response_curve_parameters')
 sanger.matrix <- load.from.taiga(data.name='aneuploidy-data-d0b9', data.version=6, data.file='sanger_matrix')
 
-drug_name_sanger = 'MPS1_IN1'
 aneuploidy.data <-load.from.taiga(data.name='aneuploidy-data-d0b9', data.version=18, data.file='aneuploidy_data_NEW_COMPACT')
-#aneuploidy.data <- load.from.taiga(data.name='aneuploidy-data-d0b9', data.version=4, data.file='aneuploidy_data_compact_CCLE')
 
 
 high_aneuploidy = aneuploidy.data[,'many_arm_events'] == TRUE
@@ -24,15 +21,15 @@ low_aneuploidy = aneuploidy.data[,'many_arm_events'] == FALSE
 both = intersect(rownames(sanger.matrix),aneuploidy.data[,'DepMap_ID'])
 sub = sanger.matrix[both,]
 
-names_high = aneuploidy.data[high_aneuploidy,'DepMap_ID'] # for crispr
+names_high = aneuploidy.data[high_aneuploidy,'DepMap_ID'] 
 in_group = row.names(sub) %in% names_high
 
-
+### only need this including lineage as covariate ###
 #sample.info <- load.from.taiga(data.name='public-19q4-93d9', data.version=21, data.file='sample_info')
 #rownames(sample.info) <- sample.info$DepMap_ID #if using CCLE names
 
-lim_res <- cdsr::run_lm_stats_limma(sub, in_group)#,covar = sample.info[rownames(sub),'lineage',drop=F])
-library(ggplot2)
-cdsr::make_volcano(lim_res, 'EffectSize', 'p.value', 'q.value',label_var='Gene')
+source("running_limma.R")
+lim_res <- run_lm_stats_limma(sub, in_group)#,covar = sample.info[rownames(sub),'lineage',drop=F])
+
 
 write.table(lim_res,file='/Users/mkazachk/Documents/Uris_paper/Limma/drug_comparisons/GDSC_NEW.csv',sep = ",",row.names = FALSE)
--- Limma/drug_comparisons/using_limma_prism.R
+++ Limma/drug_comparisons/using_limma_prism.R
@@ -9,17 +9,9 @@ install.packages('cdsr')
 
 library(taigr)
 
-# want to use secondary screen data
-#secondary.dose.response.curve.parameters <- load.from.taiga(data.name='secondary-screen-0854', data.version=13, data.file='secondary_dose_response_curve_parameters')
-
+# get prism secondary data
 secondary.matrix <- load.from.taiga(data.name='aneuploidy-data-d0b9', data.version=5, data.file='secondary_matrix')
-
-drug_name_prism = 'MPI-0479605'
-
-#rownames(secondary.dose.response.curve.parameters) <- secondary.dose.response.curve.parameters$depmap_id
-
 aneuploidy.data <- load.from.taiga(data.name='aneuploidy-data-d0b9', data.version=18, data.file='aneuploidy_data_NEW_COMPACT')
-#aneuploidy.data <- load.from.taiga(data.name='aneuploidy-data-d0b9', data.version=4, data.file='aneuploidy_data_compact_CCLE')
 
 
 high_aneuploidy = aneuploidy.data[,'many_arm_events'] == TRUE
@@ -31,12 +23,12 @@ sub = secondary.matrix[both,]
 names_high = aneuploidy.data[high_aneuploidy,'DepMap_ID'] # for crispr
 in_group = row.names(sub) %in% names_high
 
-
+### only need this if including lineage as covariate ###
 #sample.info <- load.from.taiga(data.name='public-19q4-93d9', data.version=21, data.file='sample_info')
 #rownames(sample.info) <- sample.info$DepMap_ID #if using CCLE names
 
-lim_res <- cdsr::run_lm_stats_limma(sub, in_group)#,covar = sample.info[rownames(sub),'lineage',drop=F])
-library(ggplot2)
-cdsr::make_volcano(lim_res, 'EffectSize', 'p.value', 'q.value',label_var='Gene')
+source("running_limma.R")
+lim_res <- run_lm_stats_limma(sub, in_group)#,covar = sample.info[rownames(sub),'lineage',drop=F])
 
+# save output
 write.table(lim_res,file='/Users/mkazachk/Documents/Uris_paper/Limma/drug_comparisons/prism_NEW.csv',sep = ",",row.names = FALSE)
